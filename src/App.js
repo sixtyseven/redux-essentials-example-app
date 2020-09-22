65657c414d2bfe0137ea6648ef3c580df99fac3b
Bruce <lsq***@gmail.com>
2020-09-22 22:24:06 +1000

    tmp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
-} from 'react-router-dom'
+  Redirect,
+} from 'react-router-dom';
 
-import { Navbar } from './app/Navbar'
+import { Navbar } from './app/Navbar';
 
-import { PostsList } from './features/posts/PostsList'
-import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { EditPostForm } from './features/posts/EditPostForm';
+import { SinglePostPage } from './features/posts/SinglePostPage';
 
 function App() {
   return (
@@ -22,16 +24,18 @@ function App() {
             path="/"
             render={() => (
               <React.Fragment>
-                    <AddPostForm />
+                <AddPostForm />
                 <PostsList />
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { createSelector } from 'reselect'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
 
 const selectNumOfPosts = createSelector(
-  state => state.posts,
-  posts => posts.length
-)
+  (state) => state.posts,
+  (posts) => posts.length
+);
 
 export const PostsCounter = () => {
-  const NumOfPosts = useSelector(selectNumOfPosts)
-  return <div>Number of Posts: {NumOfPosts}</div>
-}
+  const NumOfPosts = useSelector(selectNumOfPosts);
+  return <div>Number of Posts: {NumOfPosts}</div>;
+};
 
 export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
+  const posts = useSelector((state) => state.posts);
 
-  const renderedPosts = posts.map(post => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
       <p>{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
-  ))
+  ));
 
   return (
     <section>
@@ -28,5 +45,5 @@ export const PostsList = () => {
       <PostsCounter />
       {renderedPosts}
     </section>
-  )
-}
+  );
+};
